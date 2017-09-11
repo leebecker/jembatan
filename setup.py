66357cc627d60d5752bc5682c4f4d01c5904175e
Becker, Lee <lee.becker@pearson.com>
2017-09-11 16:01:49 -0600

    Adding packaging info and scripts

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='jembatan',
+      version='0.1',
+      description='Jembatan - A framework for bridging NLP libraries',
+      url='http://github.com/leebecker/jembatan',
+      author='Lee Becker',
+      license='Apache 2.0',
+      packages=find_packages(),
+      zip_safe=False)
