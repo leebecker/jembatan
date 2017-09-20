cb148b4a74e383617457bf36b10bbc1d7b8e7c55
Becker, Lee <lee.becker@pearson.com>
2017-09-19 23:34:09 -0600

    Adding HasSourceRef class/trait to type system To provide an easy mechanism to link the Jembatan Typesystem with source objects.  For example a Jembatan Token linking to the original Spacy Token.

--- jembatan/typesys/__init__.py
+++ jembatan/typesys/__init__.py
@@ -1,4 +1,14 @@
 import collections
+
+class HasSourceRef(object):
+    """ Simple Trait Class to add a source field via inheritance.
+    The source field is intends to be an easy way to communicate
+    between services
+    """
+    source = None
+
+
+
 def namedtuple_with_defaults(typename, field_names, default_values=()):
     T = collections.namedtuple(typename, field_names)
     T.__new__.__defaults__ = (None,) * len(T._fields)
@@ -10,28 +20,33 @@ def namedtuple_with_defaults(typename, field_names, default_values=()):
     return T
 
 
-class Document(namedtuple_with_defaults("Document", ["other"])): 
+class Document(namedtuple_with_defaults("Document", [])): 
     """ Top level document type """
     pass
 
-class Block(namedtuple_with_defaults("Block", ["tag", "other"])):
+class Block(namedtuple_with_defaults("Block", ["tag"])):
     pass
 
-class Sentence(namedtuple_with_defaults("Sentence", ["other"])): 
+class Sentence(namedtuple_with_defaults("Sentence", []), HasSourceRef):
     pass
 
-class Token(namedtuple_with_defaults("Token", ["lemma", "partOfSpeech", "headDependencyEdges", "childDependencyEdges", "other"])): 
+class Token(
+        namedtuple_with_defaults("Token", ["lemma", "partOfSpeech", "headDependencyEdges", "childDependencyEdges"]),
+        HasSourceRef): 
     def __repr__(self):
         return "<{}:{}>".format(self.__class__.__name__, id(self))
 
-class PartOfSpeech(namedtuple_with_defaults("PartOfSpeech", ["pos", "tag", "other"])): 
+class PartOfSpeech(namedtuple_with_defaults("PartOfSpeech", ["pos", "tag"])): 
     pass
 
-class DependencyEdge(namedtuple_with_defaults("DependencyEdge", ["label", "head", "child", "other"])): 
+class DependencyEdge(namedtuple_with_defaults("DependencyEdge", ["label", "head", "child"])): 
     pass
 
-class Entity(namedtuple_with_defaults("Entity", ["name", "salience", "type", "other"])): 
+class Entity(
+        namedtuple_with_defaults("Entity", ["name", "salience", "type"]),
+        HasSourceRef
+        ): 
     pass
 
-class NounChunk(namedtuple_with_defaults("Entity", ["type", "other"])): 
+class NounChunk(namedtuple_with_defaults("Entity", ["type"]), HasSourceRef): 
     pass
