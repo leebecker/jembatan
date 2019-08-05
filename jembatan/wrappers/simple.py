50b46cfa7df7abce66dadc7a1c5aee19f313452f
Lee Becker <lee.becker@pearson.com>
2019-08-05 14:49:11 -0600

    Making more readable

--- jembatan/wrappers/simple.py
+++ jembatan/wrappers/simple.py
@@ -1,9 +1,10 @@
+from jembatan.core.af import AnalysisFunction
 from jembatan.core.spandex import Span, Spandex
 from jembatan.typesys import Annotation
 from typing import Pattern
 
 
-class RegexMatchAnnotator:
+class RegexMatchAnnotator(AnalysisFunction):
     """
     Will turn anything matching annotation into the specified type
 
@@ -16,7 +17,7 @@ class RegexMatchAnnotator:
 
     def process(self, spndx: Spandex):
         if self.window_type:
-            windows = [pair[0] for pair in spndx.select(self.window_type)]
+            windows = [window_span for window_span, _ in spndx.select(self.window_type)]
         else:
             windows = [Span(0, len(spndx.content_string))]
 
@@ -26,12 +27,13 @@ class RegexMatchAnnotator:
 
             for match in self.match_re.finditer(window_text):
                 mbeg, mend = match.span()
-                pair = (Span(begin=window.begin+mbeg, end=window.begin+mend), self.annotation_type())
-                annotations.append(pair)
+                span = Span(begin=window.begin+mbeg, end=window.begin+mend)
+                annotation = self.annotation_type()
+                annotations.append((span, annotation))
         spndx.add_layer(self.annotation_type, annotations)
 
 
-class RegexSplitAnnotator:
+class RegexSplitAnnotator(AnalysisFunction):
 
     def __init__(self, split_re: Pattern[str], annotation_type: Annotation, window_type: Annotation=None):
         self.split_re = split_re
@@ -40,7 +42,7 @@ class RegexSplitAnnotator:
 
     def process(self, spndx: Spandex):
         if self.window_type:
-            windows = [pair[0] for pair in spndx.select(self.window_type.name)]
+            windows = [window_span for window_span, _ in spndx.select(self.window_type)]
         else:
             windows = [Span(0, len(spndx.content_string))]
 
@@ -52,22 +54,24 @@ class RegexSplitAnnotator:
 
             if not matches:
                 # no split found so make the whole window paragraph
-                pair = (Span(begin=window.begin+0, end=window.begin+len(spndx.content)), self.annotation_type())
-                annotations.append(pair)
+                span = Span(begin=window.begin+0, end=window.begin+len(spndx.content))
+                annotation = self.annotation_type()
+                annotations.append((span, annotation))
             else:
                 if matches[0].span()[0] > 0:
-                    pair = (Span(begin=window.begin+0, end=window.begin+matches[0].span()[0]), self.annotation_type())
-                    annotations.append(pair)
+                    span = Span(begin=window.begin+0, end=window.begin+matches[0].span()[0])
+                    annotation = self.annotation_type()
+                    annotations.append((span, annotation))
 
                 for m0, m1 in zip(matches[0:-1], matches[1:]):
-                    pair = (Span(begin=window.begin+m0.span()[1], end=window.begin+m1.span()[0]),
-                            self.annotation_type())
-                    annotations.append(pair)
+                    span = Span(begin=window.begin+m0.span()[1], end=window.begin+m1.span()[0])
+                    annotation = self.annotation_type()
+                    annotations.append((span, annotation))
 
                 if matches[-1].span()[1] <= len(window_text):
                     # get straggling span
-                    pair = (Span(begin=window.begin+matches[-1].span()[1], end=window.begin+len(spndx.content_string)),
-                            self.annotation_type())
-                    annotations.append(pair)
+                    span = Span(begin=window.begin+matches[-1].span()[1], end=window.begin+len(spndx.content_string))
+                    annotation = self.annotation_type()
+                    annotations.append((span, annotation))
 
         spndx.add_layer(self.annotation_type, annotations)
