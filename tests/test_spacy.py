743611ac576f35e4a5b3eac0dbd9cb7f438a8af9
Lee Becker <lee.becker@pearson.com>
2019-08-22 08:27:10 -0600

    Fixing serialization of spandex for refactor. Adding involved test case to cover json encoders.

--- tests/test_spacy.py
+++ tests/test_spacy.py
@@ -6,6 +6,30 @@ import json
 import jembatan.typesys as jemtypes
 
 
+def compare_dep_annotations(spndx, dep_parse, expected_graph, expected_pos_tags, expected_lemmas):
+    for expected, node in zip(expected_graph, spndx.select_covered(jemtypes.syntax.DependencyNode, dep_parse)):
+        node_text = spndx.spanned_text(node)
+
+        if expected[1] == "ROOT":
+            assert node.is_root
+        else:
+            assert not node.is_root
+
+        relation = node.head_edge.label
+        head_node = node.head_edge.head
+        head_text = spndx.spanned_text(head_node)
+        assert expected == (node_text, relation, head_text)
+
+        # ensure we connected the graph both ways
+        child_relations = []
+        for i, child_relation in enumerate(head_node.child_edges):
+            assert child_relation.head == head_node
+            child_node = child_relation.child
+            child_text = spndx.spanned_text(child_node)
+            child_relations.append((child_text, child_relation.label, head_text))
+        assert expected in child_relations
+
+
 def test_spacy_dep(spacy_pipeline):
     text = "John gave the ball to Mary."
     expected_graph = [
@@ -44,27 +68,7 @@ def test_spacy_dep(spacy_pipeline):
     rootnode = parses[0].root
     assert rootnode.child_edges[0].head == rootnode
 
-    for expected, node in zip(expected_graph, spndx.select(jemtypes.syntax.DependencyNode)):
-        node_text = spndx.spanned_text(node)
-
-        if expected[1] == "ROOT":
-            assert node.is_root
-        else:
-            assert not node.is_root
-
-        relation = node.head_edge.label
-        head_node = node.head_edge.head
-        head_text = spndx.spanned_text(head_node)
-        assert expected == (node_text, relation, head_text)
-
-        # ensure we connected the graph both ways
-        child_relations = []
-        for i, child_relation in enumerate(head_node.child_edges):
-            assert child_relation.head == head_node
-            child_node = child_relation.child
-            child_text = spndx.spanned_text(child_node)
-            child_relations.append((child_text, child_relation.label, head_text))
-        assert expected in child_relations
+    compare_dep_annotations(spndx, parses[0], expected_graph, expected_pos_tags, expected_lemmas)
 
 
 def test_spacy_json_serialization(spacy_pipeline):
@@ -82,18 +86,34 @@ def test_spacy_json_serialization(spacy_pipeline):
     expected_pos_tags = ['NNP', 'VBD', 'DT', 'NN', 'IN', 'NNP', '.']
     expected_lemmas = ['john', 'give', 'the', 'ball', 'to', 'mary', '.']
 
-    spndx = text_to_spandex(text)
+    spndx_in = text_to_spandex(text)
 
     spacy_analyzer = jemspacy.SpacyAnalyzer(spacy_pipeline=spacy_pipeline)
 
-    spacy_analyzer.process(spndx)
+    spacy_analyzer.process(spndx_in)
 
     encoder = jemjson.SpandexJsonEncoder()
-    
-    x = json.dumps(spndx, cls=jemjson.SpandexJsonEncoder)
-    from pprint import pprint
-    encoder.default(spndx)
-    print(x)
-    assert False
 
+    serialized_spndx_str = json.dumps(spndx_in, cls=jemjson.SpandexJsonEncoder)
 
+    spndx_out = json.loads(serialized_spndx_str, cls=jemjson.SpandexJsonDecoder)
+
+    sentences_in = spndx_in.select(jemtypes.segmentation.Sentence)
+    sentences_out = spndx_out.select(jemtypes.segmentation.Sentence)
+    assert len(sentences_in) == len(sentences_out)
+
+    for sentence_in, sentence_out in zip(sentences_in, sentences_out):
+        tokens_in = spndx_in.select_covered(jemtypes.segmentation.Token, sentence_in)
+        tokens_out = spndx_out.select_covered(jemtypes.segmentation.Token, sentence_out)
+        assert len(tokens_in) == len(tokens_out)
+
+        assert sentence_in == sentence_out
+
+        for token_in, token_out in zip(tokens_in, tokens_out):
+            assert token_in == token_out
+            assert token_in.pos == token_out.pos
+            assert token_in.lemma == token_out.lemma
+            assert spndx_in.spanned_text(token_in) == spndx_out.spanned_text(token_out)
+
+    parses = spndx_out.select(jemtypes.syntax.DependencyParse)
+    compare_dep_annotations(spndx_out, parses[0], expected_graph, expected_pos_tags, expected_lemmas)
